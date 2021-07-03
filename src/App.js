40dd310524f9b77d2ca5e13e78ef7dccc26fc9a7
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-07-04 01:32:05 +0530

    CREATE: home header component created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
+
 import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
 
 function App() {
 	return (
 		<Router>
 			<div className="App">
 				<Navbar />
-				<Switch></Switch>
+				<Switch>
+					<Home />
+				</Switch>
 			</div>
 		</Router>
 	);
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import headerImg from "../../assets/header-img.jpg";
+import "../../components/header/Header.css";
+
+const Header = () => {
+	return (
+		<div className="header-container">
+			<h1>Spread the Knowledge</h1>
+			<h2>with Everyone</h2>
+			<Link className="cta">Start Writing</Link>
+			<div className="scroll-indicator">
+				<Link to="">
+					<div className="mouse">
+						<span></span>
+					</div>
+					<div className="arrow">
+						<span></span>
+					</div>
+				</Link>
+			</div>
+			<img src={headerImg} alt="header-img" />
+		</div>
+	);
+};
+
+export default Header;
