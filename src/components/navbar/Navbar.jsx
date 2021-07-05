9fecb27bb4d3fbc14aace792d5362563595c4853
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-07-05 18:28:35 +0530

    CREATE: create blog page created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
 		return () => {
 			window.removeEventListener("scroll");
 		};
-	}, []);
+	}, [setPath]);
 
 	return (
 		<header
--- src/components/write/WriteForm.jsx
+++ src/components/write/WriteForm.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { BsPlusCircleFill } from "react-icons/bs";
+
+import "./WriteForm.css";
 
 const WriteForm = () => {
 	return (
-		<div>
-			<h1>Form</h1>
+		<div className="create-post-container">
+			<img
+				src="https://images.pexels.com/photos/2776479/pexels-photo-2776479.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+				alt="uploaded-img"
+			/>
+			<form className="create-post">
+				<div className="post-title">
+					<label htmlFor="post-img">
+						<BsPlusCircleFill />
+					</label>
+					<input
+						type="file"
+						name="post-img"
+						id="post-img"
+						accept="image/png, image/jpeg"
+					/>
+					<input
+						type="text"
+						name="post-title"
+						id="post-title"
+						placeholder="Title"
+					/>
+				</div>
+				<textarea
+					name="post-content"
+					id="post-content"
+					placeholder="Write your story..."
+					rows="auto"
+				></textarea>
+				<button type="submit">Publish</button>
+			</form>
 		</div>
 	);
 };
